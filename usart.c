26f4b54b47774d9603eaed683ee7e13f0c014a27
bemcgarvey <bemcgarvey@comcast.net>
2019-02-05 16:22:38 -0700

    Added button stub and updated tasks
    Added some more comments and streamlined some code to make easier to understand.

--- usart.c
+++ usart.c
@@ -11,6 +11,7 @@ void InitPins(void);
 void ConfigInterrupts(void);
 void ConfigPeriph(void);
 
+char rx;
 char rxBuffer[20];
 volatile int txCount;
 volatile int rxCount;
@@ -25,19 +26,29 @@ void main(void) {
     InitPins();
     ConfigPeriph();
     ConfigInterrupts();
-    TXSTA1bits.TXEN = 1;  //Turn on TX
+    TXSTA1bits.TXEN = 1; //Turn on TX
+
+    //Main loop
     while (1) {
-        while (PIR1bits.RC1IF == 0);  //Wait for a byte
-        rxBuffer[rxCount] = RCREG1; //Read the byte into the string buffer
-        if (rxBuffer[rxCount] == '\r') {  //check for CR
-            rxBuffer[rxCount] = '\0';  //Terminate the string
-            LCDClearLine(0);
-            LCDWriteLine(rxBuffer, 0);
-            rxCount = 0;
-            LATDbits.LATD1 ^= 1;
-        } else if (rxCount < 16) {
-            ++rxCount;
-            LATDbits.LATD0 ^= 1;
+        //Check the button
+        if (PORTBbits.RB0 == 0) {
+            //Put code here that executes when the button is pressed
+        }
+                
+        //Check for a received byte
+        if (PIR1bits.RC1IF == 1) { //A byte is available from USART
+            rx = RCREG1; //Read the received byte
+            rxBuffer[rxCount] = rx; //save the byte into the string buffer
+            if (rxBuffer[rxCount] == '\r') { //check for CR
+                rxBuffer[rxCount] = '\0'; //Terminate the string
+                LCDClearLine(0);
+                LCDWriteLine(rxBuffer, 0);
+                rxCount = 0;
+                LATDbits.LATD1 ^= 1; //Toggle LED D2
+            } else if (rxCount < 16) {
+                ++rxCount;
+                LATDbits.LATD0 ^= 1; //Toggle LED D1
+            }
         }
     }
 }
@@ -45,22 +56,20 @@ void main(void) {
 void InitPins(void) {
     LATD = 0; //Turn off all LED's
     TRISD = 0; //LED's are outputs
-
-    //Set TRIS bits for any required peripherals here.
+    TRISB = 0b00000001; //Set RB0 as input (this is the button)
+    
+    //Set the TX and RX pins to the correct direction
     TRISC = 0b10000000; //RC7 is RX, RC6 is TX
 
 }
 
 void ConfigInterrupts(void) {
 
-    RCONbits.IPEN = 0; //no priorities.  This is the default.
-
-    //Configure your interrupts here
-
-    //Interrupt on USART transmit buffer empty
+    //Enable interrupt when USART transmit buffer is empty
     PIE1bits.TX1IE = 1; //enable tx interrupt
-    INTCONbits.PEIE = 1; //turn on peripheral interrupts
 
+    //Turn on all enabled interrupts
+    INTCONbits.PEIE = 1; //turn on peripheral interrupts
     INTCONbits.GIE = 1; //Turn on interrupts
 }
 
@@ -83,9 +92,9 @@ void __interrupt(high_priority) HighIsr(void) {
     //Check the source of the interrupt
     if (PIR1bits.TX1IF == 1) {
         //Transmit register is empty
-        TXREG1 = txStr[txCount];
+        TXREG1 = txStr[txCount]; //Send the next character from the string
         ++txCount;
-        if (txStr[txCount] == '\0') {  //Check for the end of the string
+        if (txStr[txCount] == '\0') { //Check for the end of the string
             txCount = 0;
         }
     }
